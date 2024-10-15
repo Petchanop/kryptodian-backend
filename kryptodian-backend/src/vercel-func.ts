6d83128fb9b4bc5462ecf319b605b5f16786c0fd
petchanop <nop.hide@gmail.com>
2024-10-15 09:50:42 +0000

    wrong variable

--- kryptodian-backend/src/vercel-func.ts
+++ kryptodian-backend/src/vercel-func.ts
@@ -5,11 +5,11 @@ import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/
 import { AppModule } from './app.module';
 
 // Keep the app instance in memory for subsequent requests
-let app;
+let app : INestApplication;
 export default async function handler(req, res) {
   // Bootstrap our NestJS app on cold start
   if (!app) {
-    const app: INestApplication = await NestFactory.create(AppModule);
+    app = await NestFactory.create(AppModule);
     app.enableVersioning();
     app.useGlobalPipes(new ValidationPipe());
     const config = new DocumentBuilder()
@@ -27,13 +27,29 @@ export default async function handler(req, res) {
       operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
     };
     const document = SwaggerModule.createDocument(app, config, options);
-    SwaggerModule.setup('api', app, document); 
+    SwaggerModule.setup('api', app, document, {
+      customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
+      customJs: [
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.js",
+      ],
+      customCssUrl: [
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css",
+      ],
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+      jsonDocumentUrl: '/api/kryptodian_openapi.json',
+      yamlDocumentUrl: '/api/kryptodian_openapi.yaml',
+    });
 
     // This is important
     await app.init();
   }
-  // const adapterHost = app.get(HttpAdapterHost);
-  const httpAdapter = app.getHttpAdapter(); 
+  const adapterHost = app.get(HttpAdapterHost);
+  const httpAdapter = adapterHost.httpAdapter;
   const instance = httpAdapter.getInstance();
 
   instance(req, res);
