e109e1b4b952772d03ff30729914ae691905380d
petchanop <nop.hide@gmail.com>
2024-10-15 09:36:48 +0000

    remove unnecessary swagger set up

--- kryptodian-backend/src/vercel-func.ts
+++ kryptodian-backend/src/vercel-func.ts
@@ -28,23 +28,7 @@ export default async function handler(req, res) {
       operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
     };
     const document = SwaggerModule.createDocument(app, config, options);
-    SwaggerModule.setup('api', app, document, {
-      customfavIcon: "https://avatars.githubusercontent.com/u/6936373?s=200&v=4",
-      customJs: [
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-bundle.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.js",
-      ],
-      customCssUrl: [
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui-standalone-preset.min.css",
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.17.14/swagger-ui.css",
-      ],
-      swaggerOptions: {
-        persistAuthorization: true,
-      },
-      jsonDocumentUrl: '/api/kryptodian_openapi.json',
-      yamlDocumentUrl: '/api/kryptodian_openapi.yaml',
-    }); 
+    SwaggerModule.setup('api', app, document); 
 
     // This is important
     await app.init();
