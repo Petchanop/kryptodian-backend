194fd24e0a1eef7d6f263c4eef50e06e177f720a
petchanop <nop.hide@gmail.com>
2024-10-16 06:09:50 +0000

    add pagination for user api query

--- kryptodian-backend/src/dto/pagination/pagination.dto.ts
+++ kryptodian-backend/src/dto/pagination/pagination.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsIn, IsInt, IsString, min, Min } from "class-validator";
 
 export class PaginationDto {
     // @IsInt()
     // @Min(1)
+    @ApiProperty()
     page: number
 
     // @IsInt()
     // @Min(1)
+    @ApiProperty()
     pageSize: number
 
 }
